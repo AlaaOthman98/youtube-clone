f612386a76ce4f2075b9182f40b9a5a4aca0e81b
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-13 00:54:33 +0200

    feat: control max results per page in search service

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -1,8 +1,11 @@
+const MAX_RESULTS_PER_PAGE = 20;
 const searchApiUrl = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.VUE_APP_YOUTUBE_API_KEY}`;
 
 const getSearchResults = async (queryText) => {
   try {
-    const response = await fetch(`${searchApiUrl}&q=${queryText}`);
+    const response = await fetch(
+      `${searchApiUrl}&q=${queryText}&maxResults=${MAX_RESULTS_PER_PAGE}`
+    );
     return await response.json();
   } catch (error) {
     console.error(error);
