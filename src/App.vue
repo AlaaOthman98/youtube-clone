645589f3c3ab21c220a7594a81580823d02667e6
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-12 11:33:31 +0200

    feat: add app header scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
+    <AppHeader />
     <router-view />
   </div>
 </template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
 
 <style lang="scss"></style>
