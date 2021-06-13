bbbee2c3c0b2b62cde846a8a86a128d7f22d093b
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-13 21:08:26 +0200

    feat: create playlist model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,62 @@
+class PlaylistItem {
+  id;
+  title;
+  description;
+  publishedAt;
+  thumbnails;
+  channelTitle;
+  itemCount;
+
+  constructor(apiPlaylist) {
+    this.setModifiedPlaylist(apiPlaylist);
+  }
+
+  setModifiedPlaylist(apiPlaylist) {
+    this.id = apiPlaylist.items[0].id;
+    this.title = apiPlaylist.items[0].snippet.title;
+    this.description = apiPlaylist.items[0].snippet.description;
+    this.publishedAt = apiPlaylist.items[0].snippet.publishedAt;
+    this.thumbnails = {
+      defaultUrl: apiPlaylist.items[0].snippet.thumbnails.default.url,
+      mediumUrl: apiPlaylist.items[0].snippet.thumbnails.medium.url,
+      highUrl: apiPlaylist.items[0].snippet.thumbnails.high.url,
+    };
+    this.channelTitle = apiPlaylist.items[0].snippet.channelTitle;
+    this.itemCount = apiPlaylist.items[0].contentDetails.itemCount;
+  }
+}
+
+export { PlaylistItem };
+
+// API response hierarchy
+/*
+response {
+  items [
+    {
+      id: string,
+      snippet {
+        publishedAt: string,
+        channelId: string,
+        title: string,
+        description: string,
+        thumbnails {
+          default {
+            url
+          },
+          medium {
+            url
+          },
+          high {
+            url
+          }
+        },
+        channelTitle: string,
+      },
+      contentDetails {
+          itemCount: number
+        }
+    }
+  ]
+}
+
+*/
