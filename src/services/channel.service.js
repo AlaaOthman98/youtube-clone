2ebb05ece62ec7b6390216f8404b6be484b69ee7
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-13 20:04:45 +0200

    feat: create channel service

---
+++ src/services/channel.service.js
@@ -0,0 +1,20 @@
+import { ChannelItem } from "@/models/channel.model";
+
+const channelApiUrl = `https://www.googleapis.com/youtube/v3/channels?key=${process.env.VUE_APP_YOUTUBE_API_KEY}`;
+
+const getChannelById = async (channelId) => {
+  try {
+    const response = await fetch(
+      `${channelApiUrl}&part=snippet,brandingSettings,statistics&id=${channelId}`
+    );
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getModifiedChannelItem = async (channelId) => {
+  return new ChannelItem(await getChannelById(channelId));
+};
+
+export { getModifiedChannelItem };
