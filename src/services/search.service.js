24be181c30c06457e9b18108999414b841d89610
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-14 15:58:11 +0200

    feat: extend search service to pass page token

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -3,19 +3,20 @@ import { SearchResults } from "@/models/search-results.model";
 const MAX_RESULTS_PER_PAGE = 20;
 const searchApiUrl = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.VUE_APP_YOUTUBE_API_KEY}`;
 
-const getSearchResults = async (queryText) => {
+const getSearchResults = async (queryText, pageToken = "") => {
+  const fullApiUrl = pageToken
+    ? `${searchApiUrl}&q=${queryText}&maxResults=${MAX_RESULTS_PER_PAGE}&pageToken=${pageToken}`
+    : `${searchApiUrl}&q=${queryText}&maxResults=${MAX_RESULTS_PER_PAGE}`;
   try {
-    const response = await fetch(
-      `${searchApiUrl}&q=${queryText}&maxResults=${MAX_RESULTS_PER_PAGE}`
-    );
+    const response = await fetch(fullApiUrl);
     return await response.json();
   } catch (error) {
     console.error(error);
   }
 };
 
-const getModifiedSearchResults = async (queryText) => {
-  return new SearchResults(await getSearchResults(queryText));
+const getModifiedSearchResults = async (queryText, pageToken = "") => {
+  return new SearchResults(await getSearchResults(queryText, pageToken));
 };
 
 export { getModifiedSearchResults };
