6955b86b30da744599d614811606088d106258c6
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-14 15:56:41 +0200

    chore: install vue infinite loading library

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import InfiniteLoading from "vue-infinite-loading";
 
 import "@/styles/_global.scss";
 
 Vue.config.productionTip = false;
+Vue.use(InfiniteLoading, {});
 
 new Vue({
   router,
