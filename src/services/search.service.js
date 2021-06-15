3a370dd93b3e5bc524961c1f7d72cfc31f10d040
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-16 00:20:39 +0200

    feat: modify search service to accept type of search

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -3,16 +3,25 @@ import { SearchResults } from "@/models/search-results.model";
 const MAX_RESULTS_PER_PAGE = 20;
 const searchApiUrl = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.VUE_APP_YOUTUBE_API_KEY}`;
 
-const getSearchResults = async (queryText, pageToken = "") => {
-  const fullApiUrl = pageToken
-    ? `${searchApiUrl}&q=${queryText}&maxResults=${MAX_RESULTS_PER_PAGE}&pageToken=${pageToken}`
-    : `${searchApiUrl}&q=${queryText}&maxResults=${MAX_RESULTS_PER_PAGE}`;
+const getSearchResults = async (queryText, pageToken = "", type = "") => {
+  const fullApiUrl = getFullApiUrl(queryText, pageToken, type);
+
   try {
     const response = await fetch(fullApiUrl);
     return new SearchResults(await response.json());
   } catch (error) {
     console.error(error);
   }
+};
+
+const getFullApiUrl = (...apiArguments) => {
+  let fullApiUrl = `${searchApiUrl}&q=${apiArguments[0]}&maxResults=${MAX_RESULTS_PER_PAGE}`;
+
+  fullApiUrl += apiArguments[1] ? `&pageToken=${apiArguments[1]}` : "";
+
+  fullApiUrl += apiArguments[2] ? `&type=${apiArguments[2]}` : "";
+
+  return fullApiUrl;
 };
 
 export { getSearchResults };
