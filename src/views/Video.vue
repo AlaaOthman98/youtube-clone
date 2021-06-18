6ebf53e57bc0e050eb23d4918d507f0d471dc8d2
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-18 14:37:35 +0200

    feat: add getting video data logic

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -3,8 +3,36 @@
 </template>
 
 <script>
+import { getVideoById } from "@/services/video.service";
+
 export default {
   name: "Video",
+  data() {
+    return {
+      videoId: "",
+      videoDetails: {},
+      noVideos: false,
+    };
+  },
+  async created() {
+    this.videoId = this.$route.query.videoId;
+
+    if (this.videoId) {
+      this.videoDetails = await getVideoById(this.videoId);
+    } else {
+      this.noVideos = true;
+    }
+  },
+  async beforeRouteUpdate(to, from, next) {
+    this.videoId = to.query.videoId;
+
+    if (this.videoId) {
+      this.videoDetails = await getVideoById(this.videoId);
+    } else {
+      this.noVideos = true;
+    }
+    next();
+  },
 };
 </script>
 
