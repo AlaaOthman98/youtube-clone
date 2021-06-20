23f5e4d637c7def02cb7785733316a0e560f26ad
Alaa Othman <alaa.othman4707@gmail.com>
2021-06-20 21:49:07 +0200

    feat: create playlist items service

---
+++ src/services/playlist-items.service.js
@@ -0,0 +1,19 @@
+import router from "../router";
+
+const MAX_RESULTS_PER_PAGE = 20;
+const playlistApiUrl = `https://www.googleapis.com/youtube/v3/playlistItems?key=${process.env.VUE_APP_YOUTUBE_API_KEY}`;
+
+const getPlaylistItems = async (playlistId, nextPageToken = "") => {
+  let fullApiUrl = `${playlistApiUrl}&playlistId=${playlistId}&part=snippet&maxResults=${MAX_RESULTS_PER_PAGE}`;
+  fullApiUrl += nextPageToken ? `&pageToken=${nextPageToken}` : "";
+
+  try {
+    const response = await fetch(fullApiUrl);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    router.push({ name: "notFound" });
+  }
+};
+
+export { getPlaylistItems };
